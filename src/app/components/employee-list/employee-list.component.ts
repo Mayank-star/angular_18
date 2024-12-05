52dd9b82decf91b96b494df7daf3e34a0ff442b4
Mayank Kumar <mayankkumarcse@gmail.com>
2024-12-05 12:01:23 +0530

    component creation

---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [],
+  template: '<h1 class="secondary">Content with template</h1>',
+  styles: ['.secondary{color:orange}']
+})
+export class EmployeeListComponent {
+
+}
