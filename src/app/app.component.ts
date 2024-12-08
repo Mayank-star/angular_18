b76e1b878c50e1b7f750842896109253ff67faba
Mayank Kumar <mayankkumarcse@gmail.com>
2024-12-08 17:23:26 +0530

    reactive form creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'; 
 import { TemplateFormComponent } from './components/forms/template-form/template-form.component';
+import { ReactiveFormComponent } from './components/forms/reactive-form/reactive-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TemplateFormComponent],
+  imports: [TemplateFormComponent,ReactiveFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
